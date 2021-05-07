aca4fe8c9d3f563ab131de966f1ebc2e6968f6cd
github username <github email address>
2021-05-07 01:38:59 -0400

    worked with static methods and practice working on strings and arrays

--- index.js
+++ index.js
@@ -1,3 +1,28 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-\s']+/gi, '');
+  }
+
+  static titleize(string){
+    const bannedWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'and', 'from']; 
+
+    const stringArray = string.split(' '); 
+
+    stringArray[0] = stringArray[0][0].toUpperCase() + stringArray[0].slice(1);
+
+    for (let i = 1; i<stringArray.length; i++){
+
+      if(!bannedWords.includes(stringArray[i])){
+        stringArray[i] = stringArray[i][0].toUpperCase() + stringArray[i].slice(1)
+      }
+      
+    }
+    
+    
+    return stringArray.join(' ');
+  }
 }
